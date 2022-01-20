60a89e8ed339ecb2cac7a9a4aa13af766c76b5ff
geich <gage.eichman@ttu.edu>
2022-01-19 19:19:22 -0600

    1.3 hard coded but done

--- project1-3.py
+++ project1-3.py
@@ -2,56 +2,75 @@
 #using newtons method
 
 import numpy as np
-import sympy as sym
 import matplotlib.pyplot as plt
 
-def newton(f,Df,x0,epsilon,max_iter):
-    '''Approximate solution of f(x)=0 by Newton's method.
+def Newtons_Method(f,Df,x0,epsilon,max_iter):
 
-    Parameters
-    ----------
-    f : function
-        Function for which we are searching for a solution f(x)=0.
-    Df : function
-        Derivative of f(x).
-    x0 : number
-        Initial guess for a solution f(x)=0.
-    epsilon : number
-        Stopping criteria is abs(f(x)) < epsilon.
-    max_iter : integer
-        Maximum number of iterations of Newton's method.
-
-    Returns
-    -------
-    xn : number
-        Implement Newton's method: compute the linear approximation
-        of f(x) at xn and find x intercept by the formula
-            x = xn - f(xn)/Df(xn)
-        Continue until abs(f(xn)) < epsilon and return xn.
-        If Df(xn) == 0, return None. If the number of iterations
-        exceeds max_iter, then return None.
-
-    Examples
-    --------
-    >>> f = lambda x: x**2 - x - 1
-    >>> Df = lambda x: 2*x - 1
-    >>> newton(f,Df,1,1e-8,10)
-    Found solution after 5 iterations.
-    1.618033988749989
-    '''
     xn = x0
     for n in range(0,max_iter):
         fxn = f(xn)
+        Dfxn = Df(xn)
+
         if abs(fxn) < epsilon:
             print('Found solution after',n,'iterations.')
             return xn
-        Dfxn = Df(xn)
+
         if Dfxn == 0:
             print('Zero derivative. No solution found.')
             return None
+
         xn = xn - fxn/Dfxn
+
     print('Exceeded maximum iterations. No solution found.')
     return None
 
 
+
+f = lambda x: np.sin(x)+x-2.0
+Df = lambda x: np.cos(x)+1.0
+
+root = Newtons_Method(f,Df,1,0.0001,100)
+print(root)
+
+x = np.linspace(-10,10,1000)
+y = np.sin(x)+x-2.0
+plt.plot(x,y,color='red',label="sin(x)+x-2.0")
+plt.axvline(x=root,label= "root 1, x = {}".format(root))
+
+ax = plt.gca()
+ax.spines['top'].set_color('none')
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['bottom'].set_position('zero')
+
+plt.legend()
+plt.show()
+
+
+f1 = lambda x: np.sin(x)/x - 0.05
+Df1 = lambda x: np.cos(x)/x - np.sin(x)/(x**2)
+
+
+root1 = Newtons_Method(f1,Df1,1,0.0000001,100)
+print(root1)
+
+x = np.linspace(-10,10,1000)
+y = np.sin(x)/x-0.05
+plt.plot(x,y,color='red',label="sin(x)/x -0.05")
+
+plt.axvline(x=root1,label= "root 1, x = {}".format(root1))
+plt.axvline(x=-root1,label= "root 1, x = {}".format(-root1))
+plt.axvline(x=root1+np.pi,label= "root 2, x = {}".format(root1+np.pi))
+plt.axvline(x=-(root1+np.pi),label= "root 2, x = {}".format(-(root1+np.pi)))
+plt.axvline(x=root1+2*np.pi,label= "root 3, x = {}".format(root1+2*np.pi))
+plt.axvline(x=-(root1+2*np.pi),label= "root 3, x = {}".format(-(root1+2*np.pi)))
+
+ax = plt.gca()
+ax.spines['top'].set_color('none')
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['bottom'].set_position('zero')
+
+plt.legend()
+plt.show()
 
