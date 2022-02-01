0355e4b4c09852b843881a8bf341644da8e2003b
geich <gage.eichman@ttu.edu>
2022-02-01 12:07:43 -0600

    4.2 almost done, just need to make the graph look pretty

--- project 4-2.py
+++ project 4-2.py
@@ -24,10 +24,12 @@ import scipy as sci
 
 
 def DrivingV(a):
-    yval = np.zeros(len(a))
-    for i in range(len(a)):
-        xnR = round(a[i],0)
-        print(xnR)
+    #print(a)
+        #changed as to x's, change them back
+    yval = np.zeros(len(x))
+    for i in range(len(x)):
+        xnR = round(x[i],0)
+        #print(xnR)
         if xnR % 2 == 0:
             yval[i] = 1
         elif xnR % 2 != 0:
@@ -36,10 +38,13 @@ def DrivingV(a):
             print('something broke in DrivingV')
     return yval
 
+def Fprime(y,a,T):
+    FP = 1/T * (DrivingV(a)-y)
+    return FP
 
-
-def Eulers(y0,x):
+def Eulers(y0,x,T):
     nx = len(x)
+    #print(x)
     yval = np.zeros(nx)
     yval[0]=y0
     i = 0
@@ -47,22 +52,33 @@ def Eulers(y0,x):
         xn = x[i]
         yn = yval[i]
         dx = x[i+1]-x[i]
-        yval[i+1] = yn + Fprime(yn,xn)*dx
+        #print(Fprime(yn,xn,T))
+        yval[i+1] = yn + Fprime(yn,xn,T)[i]*dx
         i = i +1
-    print(yval)
+    #print(yval)
     return(yval)
 
-def Fprime(y,a):
-    FP = -3 * y + np.cos(a)
-    return FP
 
 
-y0 =1
-dx = .1
+y0 = 0
+dx = .01
+
+T1 = 1
+T2 = 0.1
+T3 = 0.01
 
 #making x values and y values
-x = np.arange(-10,10,dx)
+x = np.arange(0,10,dx)
 y = DrivingV(x)
+y1 = Eulers(y0,x,T1)
+y2 = Eulers(y0,x,T2)
+y3 = Eulers(y0,x,T3)
 
-plt.plot(x,y)
+fig = plt.figure()
+fig.patch.set_facecolor('xkcd:mint green')
+plt.plot(x,y,"r")
+plt.plot(x,y1,"purple")
+plt.plot(x,y2,"blue")
+plt.plot(x,y3,"black")
+
 plt.show()
